49895ad3dee9f926fa5ba07d3f84dbbac14cf7ff
Serhii Plis <serzh2577@gmail.com>
2024-05-04 21:47:53 +0300

    add update favorite

--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -5,23 +5,25 @@ import {
   deleteContact,
   createContact,
   updateContact,
+  updateStatusContact,
 } from "../controllers/contactsControllers.js";
 import validateBody from "../helpers/validateBody.js";
 import {
   createContactSchema,
   updateContactSchema,
+  updateStatusContactSchema,
 } from "../schemas/contactsSchemas.js";
 
 const contactsRouter = express.Router();
-
 contactsRouter.get("/", getAllContacts);
-
 contactsRouter.get("/:id", getOneContact);
-
 contactsRouter.delete("/:id", deleteContact);
-
 contactsRouter.post("/", validateBody(createContactSchema), createContact);
-
 contactsRouter.put("/:id", validateBody(updateContactSchema), updateContact);
+contactsRouter.patch(
+  "/:id/favorite",
+  validateBody(updateStatusContactSchema),
+  updateStatusContact
+);
 
 export default contactsRouter;
